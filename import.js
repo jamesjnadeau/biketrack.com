ce39d1ce7a8fc6d9f0406ac0bca9a70dcd5cdd40
James Nadeau <james.nadeau@competitive.com>
2019-10-10 14:34:13 -0400

    :tada: initial working scrape

---
+++ import.js
@@ -0,0 +1,35 @@
+
+ 
+var scrape = require('website-scraper');
+var rimraf = require('rimraf');
+var sitePath =  __dirname + '/site';
+
+
+rimraf(sitePath, function () {
+  var options = {
+    urls: ['https://biketrack.com/'],
+    directory: sitePath,
+    recursive: true,
+    maxRecursiveDepth: 5,
+    onResourceSaved: (resource) => {
+    	console.log(`Resource ${resource} was saved to fs`);
+    },
+    urlFilter: function(url){
+      /*return url.indexOf('https://www.youtube.com') === -1
+        && url.indexOf('https://accounts.google.com/') === -1
+        && url.indexOf('https://secure.loom3otto.com' === -1);
+      */
+     return url.indexOf('biketrack.com/') !== -1;
+    },
+  };
+  console.log(options);
+   
+  // with promise
+  scrape(options).then((result) => {
+      console.log(result)
+  }).catch((err) => {
+      console.error(err)
+  });
+});
+
+
